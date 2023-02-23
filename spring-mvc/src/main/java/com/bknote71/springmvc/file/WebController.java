0fc7b97447c57f89c8cf356c8e60f8ddc5c0b31a
bknote71 <bknote71@gmail.com>
2023-02-23 13:44:10 +0900

    mvc-file

--- spring-mvc/src/main/java/com/bknote71/springmvc/file/WebController.java
+++ spring-mvc/src/main/java/com/bknote71/springmvc/file/WebController.java
@@ -1,5 +1,15 @@
 package com.bknote71.springmvc.file;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class WebController {
+
+    @GetMapping("/")
+    public @ResponseBody String fileHome() {
+        return "file-home";
+    }
 }
